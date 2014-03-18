c019c9f9ac053325d9a66376530a393712f49d9b
SuryaSankar <suryashankar.m@gmail.com>
2014-03-18 18:17:33 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orca::Application.config.secret_key_base = '2b90b588515b3669b510c420629222954a76c922ce628afbccfc1ec9678f22f13d59a0ace8e3d288dcec17e6a40cdf20bdcec3728e37683d2e3ab068accf8fc5'
